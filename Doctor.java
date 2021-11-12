5c7d489f6a159f9a56a281cb9c9a1eb070f57bb5
acayer <55299919+acayer@users.noreply.github.com>
2021-11-11 18:11:11 -0700

    Add files via upload

---
+++ Doctor.java
@@ -0,0 +1,78 @@
+package application;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Doctor implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+
+	private ArrayList<Nurse> nNurses;
+	private String docName;
+
+	public Doctor(String inName)
+	{
+		docName = inName;
+		nNurses = new ArrayList<Nurse>();
+	}
+
+	public void createPatientAccount(String inUser, String inPassword, String inBirthday, String inNurse)
+	{
+		for(int i = 0; i < nNurses.size(); i++)
+		{
+			if(nNurses.get(i).getUsername().equals(inNurse))
+			{
+				nNurses.get(i).addPatient(new Patient(inUser, inPassword, inBirthday));
+			}
+		}
+	}
+
+	public void addNurse(Nurse in_nurse)
+	{
+		nNurses.add(in_nurse);
+	}
+
+	public void addPatientPhysical(String inUser, String inPhysical)
+	{
+		for(int i = 0; i < nNurses.size(); i++)
+		{
+			for(int j = 0; j < nNurses.get(i).nPatients.size(); j++)
+			{
+				Patient pat = nNurses.get(i).nPatients.get(j);
+				if(pat.getUsername().equals(inUser))
+				{
+					pat.addPhysicals(inPhysical);
+				}
+			}
+		}
+	}
+
+	public void setPrescription(String inUser, String inPrescription)
+	{
+		for(int i = 0; i < nNurses.size(); i++)
+		{
+			for(int j = 0; j < nNurses.get(i).nPatients.size(); j++)
+			{
+				Patient pat = nNurses.get(i).nPatients.get(j);
+				if(pat.getUsername().equals(inUser))
+				{
+					pat.setPrescription(inPrescription);
+				}
+			}
+		}
+	}
+
+	public void sendPrescription(String inUser)
+	{
+		System.out.println("Prescirption has been sent!");
+	}
+
+	public String getName()
+	{
+		return docName;
+	}
+
+	public ArrayList<Nurse> getNurses()
+	{
+		return nNurses;
+	}
+}
---
+++ saveSystem.java
@@ -0,0 +1,68 @@
+package application;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class saveSystem
+{
+	public saveSystem()
+	{
+		//initialize class for accessing methods, no parameters to initialize
+	}
+	public void saveFile(Object saveDoctor)
+	{
+		try
+		{
+			FileOutputStream outFile = new FileOutputStream(new File("save_file.txt"));
+			ObjectOutputStream outStream = new ObjectOutputStream(outFile);
+			outStream.writeObject(saveDoctor);
+			outStream.close();
+			System.out.println("The Object was succesfully written to a file. ");
+        }
+		catch (FileNotFoundException e)
+		{
+			System.out.println("File not found");
+		}
+		catch (IOException e)
+		{
+			System.out.println("Error initializing stream");
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+        }
+	}
+
+	public Object loadFile()
+	{
+		try
+		{
+			FileInputStream inFile = new FileInputStream(new File("save_file.txt"));
+			ObjectInputStream inStream = new ObjectInputStream(inFile);
+			Object obj = inStream.readObject();
+			System.out.println("The Object has been read from the file");
+			inStream.close();
+			return obj;
+	    }
+		catch (FileNotFoundException e)
+		{
+			System.out.println("File not found");
+			return null;
+		}
+		catch (IOException e)
+		{
+			System.out.println("Error initializing stream");
+			return null;
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+			return null;
+		}
+	}
+
+}
