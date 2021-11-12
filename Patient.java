db34c38fdddb8ae2128573c08567c89418e42d34
acayer <55299919+acayer@users.noreply.github.com>
2021-11-12 14:54:01 -0700

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -1,7 +1,11 @@
 package application;
 import java.util.*;
+import java.io.Serializable;
 
-public class Patient extends User {
+public class Patient extends User implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
 	ArrayList<String> bloodPressure;
 	ArrayList<Double> bodyTemp;
 	ArrayList<Double> weight;
@@ -14,15 +18,15 @@ public class Patient extends User {
 	ArrayList<String> healthConcerns;
 	ArrayList<String> physicals;
 	int age;
-	
+
 	Doctor myDoctor;
 	Nurse myNurse;
-	
+
 	Patient(String username, String password, String birthday) {
-		
+
 		super(username, password);
 		this.birthday = birthday;
-		
+
 		bloodPressure = new ArrayList<>();
 		bodyTemp = new ArrayList<>();
 		weight = new ArrayList<>();
@@ -31,103 +35,103 @@ public class Patient extends User {
 		healthConcerns = new ArrayList<>();
 		physicals = new ArrayList<>();
 	}
-	
+
 	public void setInsurance(String inInsurance) {
 		insurance = inInsurance;
 	}
-	
+
 	public void setPhoneNumber(String inPhone) {
 		phoneNumber = inPhone;
 	}
-	
+
 	public void setEmail(String inEmail) {
 		emailAddress = inEmail;
 	}
-	
+
 	public void addAllergens(String inPatientAllergens) {
 		allergens.add(inPatientAllergens);
 	}
-	
+
 	public void addHealth(String inHealth) {
 		healthConcerns.add(inHealth);
 	}
-	
+
 	public void setAge(int inAge) {
 		age = inAge;
 	}
-	
+
 	public void addPhysicals(String inPhys) {
 		physicals.add(inPhys);
 	}
-	
+
 	public void setVitals(String inBloodPressure, double inBodyTemp, double inWeight, double inHeight) {
 		bloodPressure.add(inBloodPressure);
 		bodyTemp.add(inBodyTemp);
 		weight.add(inWeight);
 		height.add(inHeight);
 	}
-	
+
 	public String getInsurance() {
 		return insurance;
 	}
-	
+
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
-	
+
 	public String getEmail() {
 		return emailAddress;
 	}
-	
+
 	public ArrayList<String> getAllergens() {
 		return allergens;
 	}
-	
+
 	public ArrayList<String> getHealthConcerns() {
 		return healthConcerns;
 	}
-	
+
 	public ArrayList<String> getPhysicals() {
 		return physicals;
 	}
-	
+
 	public int getAge() {
 		return age;
 	}
-	
+
 	public ArrayList<String> getBloodPressure() {
 		return bloodPressure;
 	}
-	
+
 	public ArrayList<Double> getBodyTemp() {
 		return bodyTemp;
 	}
-	
+
 	public ArrayList<Double> getWeight() {
 		return weight;
 	}
-	
+
 	public ArrayList<Double> getHeight(){
 		return height;
 	}
-	
-	public void message(String username, Message message) {
-		
-		this.messageHistory.add(message);
-		
-		if(username.equals(myDoctor.getUsername())) {
-			
-			myDoctor.getMessageHistory.add(message);
-			
-		} else if(username.equals(myNurse.getUsername())) {
 
-			myNurse.getMessageHistory.add(message);
-			
-		} else {
-			
+	public void message(String username, Message message)
+	{
+
+		this.messageHistory.add(message);
+
+		if(username.equals(myDoctor.getUsername()))
+		{
+			myDoctor.getMessageHistory().add(message);
+		}
+		else if(username.equals(myNurse.getUsername()))
+		{
+			myNurse.getMessageHistory().add(message);
+		}
+		else
+		{
 			// invalid recipient... gotta handle this somehow
 			// could have this return false and make an error message pop up
-			
 		}
 	}
 }
