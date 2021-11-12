02514eb874ff66787dc412d9dfc4d3c13f491825
Julian Najera-Amarillas <76508341+JulianNajera-Amarillas@users.noreply.github.com>
2021-11-11 16:12:38 -0800

    Update Nurse.java

--- Nurse.java
+++ Nurse.java
@@ -5,17 +5,22 @@ import java.util.*;
 
 public class Nurse extends User {
 	
+	// create arrayList of Patients
 	protected ArrayList<Patient> inPatients;
 	
+	// create a new patient account by calling the super constructor within the method
 	createPatientAccount(String userName, String password, String inBirth) {
 		super.createPatientAccount(userName, password, inBirth);
 	}
 	
+	
 	void addPatient(Patient inPatient) {
-		
+		// add Patient object to the arraylist inPatients
 		inPatients.add(inPatient);
 	}
 	
+	
+	// add the Patient's data to the Patient object that is passed in to the method
 	void addPatientData(Patient inPatient, String inBloodPressure, double inBodyTemperature, String inWeight, double inHeight, String inAllergens, String inHealth) {
 		inPatient.Vitals.bloodPressure = inBloodPressure;
 		inPatient.Vitals.bodyTemp = inBodyTemperature;
@@ -25,6 +30,7 @@ public class Nurse extends User {
 		inPatient.healthConcerns.add(inHealth);
 	}
 	
+	// return the inPatients arrayList
 	ArrayList<Patient> getPatients() {
 		return inPatients;
 	}
