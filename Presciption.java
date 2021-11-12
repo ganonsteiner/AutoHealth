2bf463412f8d662f81ee4fa7b57f7cf61bb5e68c
djonesmi <89475809+djonesmi@users.noreply.github.com>
2021-11-11 17:02:18 -0700

    Prescription file
    The prescription file.

---
+++ Presciption.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Presciption {
+	String prescriptionName;
+	String address;
+	
+	public Presciption(String inName, String inAdd ) {
+		prescriptionName = inName;
+		address = inAdd;
+	}
+}
