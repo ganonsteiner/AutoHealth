9b8d9f37a09a4ccdd48c803d648d1f3e99d4e451
djonesmi <89475809+djonesmi@users.noreply.github.com>
2021-11-13 15:41:26 -0700

    Update User.Java
    Added serializable

--- User.java
+++ User.java
@@ -1,8 +1,10 @@
 package application;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class User {
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 	protected String username;
 	protected String password;
