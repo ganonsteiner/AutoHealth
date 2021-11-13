af4452f8978a323a98cc3781095f1a95e5c5f1de
djonesmi <89475809+djonesmi@users.noreply.github.com>
2021-11-13 13:23:00 -0700

    Updated Contol.java
    Updated Contol

--- Control.java
+++ Control.java
@@ -36,6 +36,29 @@ public class Control {
 	private String nurseUser = "jbiggums123";
 	private Doctor doctor1 = new Doctor(doctorUser, doctorPass, doctorName );
 	private Nurse nurse1 = new Nurse(nurseUser, nursePass, nurseName, doctor1);
+	
+	
+	public void register(ActionEvent event) throws IOException {
+		   
+		  String name = userName.getText();
+		  String pass = newPass.getText();
+		  String birth = birthday.getText();
+		
+		   FXMLLoader loader = new FXMLLoader(getClass().getResource("Welcome.fxml"));
+		   root = loader.load();
+		   
+		  Patient patient1 = new Patient(name, pass, birth, nurse1);
+		  System.out.print(nurse1.getPatients().get(0).getPassword());
+		  Control scene3Controller = loader.getController();
+		  scene3Controller.welcomeMessage(patient1.getUsername());
+		  
+		  
+		   
+		   stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		   scene = new Scene(root);
+		   stage.setScene(scene);
+		   stage.show();
+		}
   
    public void login(ActionEvent event) throws IOException {
 	   String pass = passWord.getText();
@@ -54,55 +77,50 @@ public class Control {
 	   scene = new Scene(root);
 	   stage.setScene(scene);
 	   stage.show();
-	   } else {
-
+	   }
+	   else if(userN.equalsIgnoreCase(nurseUser) && pass.equals(nursePass)) {
+		   FXMLLoader loader = new FXMLLoader(getClass().getResource("Nurse.fxml"));
+		   root = loader.load();
+		   
+		   //Control2 scene2Controller = loader.getController();
+		   //scene2Controller.changeChoiceBox(nurse1.getPatients());
+		   
+		   stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		   scene = new Scene(root);
+		   stage.setScene(scene);
+		   stage.show();
+		}else {
+			Patient currentUser;
 			Nurse tempNurse;
-			Patient tempPatient;
-
-			for (int i = 0; i < doctor1.getNurses().size(); i++) {
-
+			
+			for(int i = 0; i < doctor1.getNurses().size(); i++) {
+				
 				tempNurse = doctor1.getNurses().get(i);
+				
+				for(int j = 0; j < tempNurse.getPatients().size(); j++) {
+					
+					Patient tempPatient = tempNurse.getPatients().get(j);
+					
+					if(tempPatient.getUsername().equals(userN) && tempPatient.getPassword().equals(pass)) {
+						currentUser = tempPatient;
 
-				for (int j = 0; j < tempNurse.getPatients().size(); j++) {
-
-					tempPatient = tempNurse.getPatients().get(j);
-
-					if (tempPatient.getUsername().equals(userN) && tempPatient.getPassword().equals(pass)) {
-						Patient currentUser = tempPatient;
-						System.out.println("patient acquired");
-						FXMLLoader loader = new FXMLLoader(getClass().getResource("Patient.fxml"));
-						root = loader.load();
-						stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-						scene = new Scene(root);
-						stage.setScene(scene);
-						stage.show();
+						   FXMLLoader loader = new FXMLLoader(getClass().getResource("Patient.fxml"));
+						   root = loader.load();
+						   
+//						   Control2 scene2Controller = loader.getController();
+//						   scene2Controller.displayName(userN);
+						   
+						   stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+						   scene = new Scene(root);
+						   stage.setScene(scene);
+						   stage.show();
 					}
-				}
 		}
-// 	   else if(userN.equalsIgnoreCase(nurseUser) && pass.equals(nursePass)) {
-// 		   FXMLLoader loader = new FXMLLoader(getClass().getResource("Nurse.fxml"));
-// 		   root = loader.load();
-		   
-// 		   //Control2 scene2Controller = loader.getController();
-// 		   //scene2Controller.changeChoiceBox(nurse1.getPatients());
-		   
-// 		   stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-// 		   scene = new Scene(root);
-// 		   stage.setScene(scene);
-// 		   stage.show();
-// 		}else {
-// 				   FXMLLoader loader = new FXMLLoader(getClass().getResource("Patient.fxml"));
-// 				   root = loader.load();
-				   
-// 				   Control2 scene2Controller = loader.getController();
-// 				   scene2Controller.displayName(userN);
-				   
-// 				   stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-// 				   scene = new Scene(root);
-// 				   stage.setScene(scene);
-// 				   stage.show();
-// 				   }
+			
+			
+				   }
 		}
+   }
    
    public void signUp(ActionEvent event) throws IOException {
 	   
@@ -119,27 +137,7 @@ public class Control {
 	   stage.setScene(scene);
 	   stage.show();
 	   }
-	public void register(ActionEvent event) throws IOException {
-		   
-		  String name = userName.getText();
-		  String pass = newPass.getText();
-		  String birth = birthday.getText();
-		
-		   FXMLLoader loader = new FXMLLoader(getClass().getResource("Welcome.fxml"));
-		   root = loader.load();
-		   
-		  Patient patient1 = new Patient(name, pass, birth, nurse1);
-		  
-		  Control scene3Controller = loader.getController();
-		  scene3Controller.welcomeMessage(patient1.getUsername());
-		  
-		  
-		   
-		   stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		   scene = new Scene(root);
-		   stage.setScene(scene);
-		   stage.show();
-		}
+
 	   public  void displayName(String userName)  
 	    {
 	    	this.userName.setText(userName);
